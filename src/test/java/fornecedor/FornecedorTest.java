1fb4e2050d971ec339c219f152284f195dd649fc
leosilvadev <leonardo@vaif.com.br>
2013-09-23 22:03:53 -0300

    .

--- src/test/java/fornecedor/FornecedorTest.java
+++ src/test/java/fornecedor/FornecedorTest.java
@@ -3,31 +3,57 @@ package fornecedor;
 import java.io.FileNotFoundException;
 import java.io.IOException;
 
+import org.junit.Assert;
+import org.junit.Ignore;
 import org.junit.Test;
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
 
+import util.BaseTest;
 import util.Propriedade;
 
-public class FornecedorTest {
+public class FornecedorTest extends BaseTest {
 
 	@Test
-	public void testFirefox() throws FileNotFoundException, IOException{
-		testFlow(new FirefoxDriver());
+	public void testFirefox() throws FileNotFoundException, IOException, InterruptedException{
+		FirefoxDriver firefoxDriver = new FirefoxDriver();
+		init(firefoxDriver);
+		testFlow(firefoxDriver);
 	}
 	
 	@Test
-	public void testChrome() throws FileNotFoundException, IOException{
+	@Ignore
+	public void testChrome() throws FileNotFoundException, IOException, InterruptedException{
 		System.setProperty("webdriver.chrome.driver", "C:\\Program Files (x86)\\Google\\Chrome\\Application\\chrome.exe");
 		testFlow(new ChromeDriver());
 	}
 	
-	public void testFlow(WebDriver driver) throws FileNotFoundException, IOException{
+	public void testFlow(WebDriver driver) throws FileNotFoundException, IOException, InterruptedException{
 		String urlAcesso = Propriedade.getPropriedade("url.listafornecedor");
 		driver.get(urlAcesso);
-	}
-	
-	
+		driver.findElement(By.className("btnNovoFornecedor")).click();
+		
+		Thread.sleep(2000);
+		
+		driver.findElement(By.className("txtNome")).sendKeys("Fornecedor Teste");
+		Thread.sleep(1000);
+		driver.findElement(By.className("txtTelefone")).sendKeys("(12)12345432");
+		Thread.sleep(1000);
+		driver.findElement(By.className("txtEndereco")).sendKeys("Rua Fulano teste selenium");
+		Thread.sleep(1000);
+		driver.findElement(By.className("txtEmail")).sendKeys("fulano@teste.com.br");
+		Thread.sleep(1000);
+		driver.findElement(By.className("btnSalvar")).click();
+
+		Thread.sleep(2000);
+		
+		Assert.assertEquals("fulano@teste.com.br", driver.findElement(By.className("item_email_fornecedor")).findElement(By.tagName("div")).getText());
+		Assert.assertEquals(1, driver.findElements(By.className("item_email_fornecedor")).size());
+		
+		Thread.sleep(2000);
+		driver.close();
+	}	
 
 }
---
+++ src/test/java/util/BaseTest.java
@@ -0,0 +1,18 @@
+package util;
+
+import java.io.FileNotFoundException;
+import java.io.IOException;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebDriverBackedSelenium;
+
+public class BaseTest {
+	
+	private WebDriverBackedSelenium selenium;
+	
+	public void init(WebDriver driver) throws FileNotFoundException, IOException{
+		selenium = new WebDriverBackedSelenium(driver, Propriedade.getPropriedade("url.base"));
+		selenium.open(Propriedade.getPropriedade("url.app.name"));
+	}
+
+}
